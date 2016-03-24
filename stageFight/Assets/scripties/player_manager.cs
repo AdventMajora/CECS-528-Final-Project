fe19ae8fa16f43c283f8dd15712e7503455f0034
AdventMajora <enorvellcloud@gmail.com>
2016-03-24 14:44:10 -0400

    Initial Commit

---
+++ stageFight/Assets/scripties/player_manager.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+
+/// <summary>
+/// Player controller and behavior
+/// </summary>
+public class player_manager : MonoBehaviour
+{
+	/// <summary>
+	/// 1 - The speed of the ship
+	/// </summary>
+	public Vector3 speed = new Vector3(10,10,10);
+	public Vector3 rot = new Vector3(0,0,0);
+	
+	public Vector3 movement;
+    public float inputX = 0f;
+	public float inputZ = 0f;
+	
+	private Animator animator;
+ 
+    void Start()
+    {
+		animator = this.GetComponent<Animator>();
+    }
+	
+	void Update()
+	{
+		animator.SetInteger("moving", 0);
+        inputX = 0f;
+		inputZ = 0f;
+        
+        if (Input.GetKey(KeyCode.Escape)) {
+            Debug.Log("Detected key code");
+            Application.Quit();
+        }
+        
+        
+            
+        if (Input.GetKey(KeyCode.W)) {
+            inputX = 1f;
+			animator.SetInteger("dir", 0);
+			animator.SetInteger("moving", 1);
+        }
+        if (Input.GetKey(KeyCode.A)) {
+            inputZ = 1f;
+			animator.SetInteger("dir", 1);
+			animator.SetInteger("moving", 1);
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            inputX = -1f;
+			animator.SetInteger("dir", 2);
+			animator.SetInteger("moving", 1);
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            inputZ = -1f;
+			animator.SetInteger("dir", 3);
+			animator.SetInteger("moving", 1);
+        }
+            
+		// 4 - Movement per direction
+		movement = new Vector3(
+			inputX,
+			0f,
+            inputZ);
+		
+	}
+    
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+    }
+	
+	void FixedUpdate()
+	{
+		
+		transform.position += movement;
+		transform.rotation = Quaternion.Euler(0,90,0);
+        
+	}
+}
