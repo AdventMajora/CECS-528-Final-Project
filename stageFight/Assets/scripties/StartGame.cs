70c71031eb8217d8cca4dc20919b8f5fbc93a50c
AdventMajora <enorvellcloud@gmail.com>
2016-04-22 16:37:33 -0400

    Start Screen!

---
+++ stageFight/Assets/scripties/StartGame.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartGame : MonoBehaviour {
+
+	public void StartScene(){
+		Application.LoadLevel ("StageFight");
+	}
+
+	public void ExitGame(){
+		Application.Quit ();
+	}
+
+
+}
