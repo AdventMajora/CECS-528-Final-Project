b374a1ec700e7d926e6d95f61783a539c634d21b
AdventMajora <enorvellcloud@gmail.com>
2016-04-23 19:51:43 -0400

    updates

---
+++ stageFight/Assets/scripties/TriggerDetector.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerDetector : MonoBehaviour {
+
+	private GameManager manager;
+
+	// Use this for initialization
+	void Start () {
+		manager = GameObject.Find ("Camera").GetComponent<GameManager> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider myTrigger) {
+		if (manager.isGamePlaying ()) {
+			if (myTrigger.gameObject.CompareTag ("Player")) {
+				if (myTrigger.gameObject.name == "Player red") {
+					manager.endGame (2);
+				} else {
+					manager.endGame (1);
+				}
+			}
+		}
+	}
+}
